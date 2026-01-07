a6227eae2c7c7eb5dd622e319e31003a50b8eb9e
mohammadr09 <mohammadnyc96@gmail.com>
2026-01-06 19:08:49 -0500

    Change base url for Copy Feature

--- src/lib/modules/copyLinkToUserClipboard.ts
+++ src/lib/modules/copyLinkToUserClipboard.ts
@@ -1,5 +1,5 @@
-// const base = "https://www.internyl.org/pages/internships/";
-const base = "http://localhost:3000/pages/internships"
+const base = "https://www.internyl.org/pages/internships/";
+// const base = "http://localhost:3000/pages/internships"
 
 export const copyLinkToUserClipboard = (link: string) => {
     const fullLink = `${base}#${link}`;
