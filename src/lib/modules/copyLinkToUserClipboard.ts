0647e6354c99590f7edeb65a3366c8a50d606d54
mohammadr09 <mohammadnyc96@gmail.com>
2026-01-06 19:07:30 -0500

    Share Button for Internships Added
    NOTE: Add a feature that highlights the internship card

---
+++ src/lib/modules/copyLinkToUserClipboard.ts
@@ -0,0 +1,8 @@
+// const base = "https://www.internyl.org/pages/internships/";
+const base = "http://localhost:3000/pages/internships"
+
+export const copyLinkToUserClipboard = (link: string) => {
+    const fullLink = `${base}#${link}`;
+
+    navigator.clipboard.writeText(fullLink);
+}
