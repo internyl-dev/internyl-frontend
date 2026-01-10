4ee9c15b34e0557c7a05b4643ddddf1bd1d0bb83
Efrat Hossain <161238844+cold-muffin@users.noreply.github.com>
2026-01-10 10:09:46 -0500

    Revert "`refactor` -> `dev`; Share Feature v1"

--- src/lib/modules/copyLinkToUserClipboard.ts
+++
@@ -1,8 +0,0 @@
-const base = "https://www.internyl.org/pages/internships/";
-// const base = "http://localhost:3000/pages/internships"
-
-export const copyLinkToUserClipboard = (link: string) => {
-    const fullLink = `${base}#${link}`;
-
-    navigator.clipboard.writeText(fullLink);
-}
